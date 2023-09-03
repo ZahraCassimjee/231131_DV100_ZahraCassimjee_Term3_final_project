fe3be80f84ac44cef043b60f25ab2f84f6c38b0c
ZahraCassimjee <231131@virtualwindow.co.za>
2023-09-03 19:30:17 +0200

    edit

--- js/index.js
+++ js/index.js
@@ -1,11 +1,10 @@
 const trips = [
     {
-        
         name: "Caribbean Cruise",
         destination: "Caribbean Islands",
         price: "12000",
         dateOfDeparture: "2023-09-15",
-        duration: "7 days",
+        duration: 5,
         tripCode: "CAR123",
         image: "card 2.jpg"
     },
@@ -14,7 +13,7 @@ const trips = [
         destination: "Mediterranean Sea",
         price: "15000",
         dateOfDeparture: "2023-08-20",
-        duration: "10 days",
+        duration: 10,
         tripCode: "MED456",
         image: "card 8.jpg"
     },
@@ -23,7 +22,7 @@ const trips = [
         destination: "Alaska, USA",
         price: "18000",
         dateOfDeparture: "2023-07-10",
-        duration: "14 days",
+        duration: 14,
         tripCode: "ALA789",
         image: "card 3.jpg"
     },
@@ -32,7 +31,7 @@ const trips = [
         destination: "Hawaii, USA",
         price: "14000",
         dateOfDeparture: "2023-08-05",
-        duration: "8 days",
+        duration: 8,
         tripCode: "TRO101",
         image: "card 6.jpeg"
     },
@@ -41,7 +40,7 @@ const trips = [
         destination: "Various European Cities",
         price: "25000",
         dateOfDeparture: "2023-09-01",
-        duration: "12 days",
+        duration: 12,
         tripCode: "EUR202",
         image: "card 5.jpg"
     },
@@ -50,7 +49,7 @@ const trips = [
         destination: "Southeast Asia",
         price: "22000",
         dateOfDeparture: "2023-10-10",
-        duration: "15 days",
+        duration: 15,
         tripCode: "ASI303",
         image: "card 4.jpg"
     },
@@ -59,7 +58,7 @@ const trips = [
         destination: "Australia",
         price: "19000",
         dateOfDeparture: "2023-11-20",
-        duration: "11 days",
+        duration: 11,
         tripCode: "AUS404",
         image: "card 9.jpg"
     },
@@ -68,7 +67,7 @@ const trips = [
         destination: "South America",
         price: "23000",
         dateOfDeparture: "2023-12-05",
-        duration: "13 days",
+        duration: 13,
         tripCode: "SAM505",
         image: "card 7.jpg"
     },
@@ -77,79 +76,82 @@ const trips = [
         destination: "Arctic Circle",
         price: "28000",
         dateOfDeparture: "2024-01-15",
-        duration: "18 days",
+        duration: 18,
         tripCode: "ARC606",
         image: "card 10.jpg"
     }
 ];
 
-$(document).ready(function() {
-    // ... (your existing code)
+// filter
 
-    // Add an event listener for the price filter dropdown
-    $("#price-filter").change(function() {
-        const sortByPrice = $("#price-filter").val();
-        if (sortByPrice === "high-to-low") {
-            trips.sort((a, b) => b.price - a.price);
-        } else if (sortByPrice === "low-to-high") {
-            trips.sort((a, b) => a.price - b.price);
-        }
-        // Call loadTripOptions to re-render the sorted trips
-        loadTripOptions();
-    });
-});
-    
-    
-    function loadTripOptions () {
-        for (let i = 0; i < trips.length; i++) {
-            console.log(trips[i]);
-    
-            $("#tripsContainer").append($("#tripCardTemplate").html());
-    
-            let currentChild = $("#tripsContainer").children().eq(i+1);
-    
-            $(currentChild).find(".card-img-top").attr('src','/assets/' + trips[i].image);
-    
-            $(currentChild).find("#nameText").text(trips[i].name);
-    
-            $(currentChild).find("#priceText").text('R ' + trips[i].price);
-    
-            $(currentChild).find("#destinationText").text(trips[i].destination);
-    
-            $(currentChild).find("#durationText").text(trips[i].duration);
+$(document).ready(function () {
+    const tripContainer = $("#tripsContainer");
 
-            $(currentChild).find("#dateOfDepartureTextext").text( trips[i].dateOfDeparture);
+    function displayTrips(filteredTrips) {
+        tripContainer.empty(); // Clear existing trips
 
-            $(currentChild).find("#tripCodeText").text(trips[i].tripCode);
-            
-            
+        filteredTrips.forEach(function (trip) {
+                const card = `
+                    <div class="card mb-3">
+                        <div class="card-body">
+                            <h5 class="card-title">${trip.name}</h5>
+                            <p class="card-text"><strong>Destination:</strong> ${trip.destination}</p>
+                            <p class="card-text"><strong>Price:</strong> R ${trip.price}</p>
+                            <p class="card-text"><strong>Date of Departure:</strong> ${trip.dateOfDeparture}</p>
+                            <p class="card-text"><strong>Duration:</strong> ${trip.duration} days</p>
+                            <p class="card-text"><strong>Trip Code:</strong> ${trip.tripCode}</p>
+                        </div>
+                    </div>
+                `;
+                tripContainer.append(card);
+        });
+    }
+
+    function filterTrips(criteria) {
+        switch (criteria) {
+            case "all": displayTrips(trips); break;
+            case "short": displayTrips(trips.filter( (trip) => trip.duration <= 5)); break;
+            case "long": displayTrips(trips.filter( (trip) => trip.duration > 5)); break;
+            default:
+                break;
         }
     }
-    
-    
-    $("#plantsContainer").on('click', '.card', function(){
-    
-      // Toggle the price & description text
-      $(this).find("#priceText").toggle();
-      $(this).find("#descriptionText").toggle();
-    
-      // Resize the image to fit the additional content
-      $(this).find(".card-img-top").toggleClass("small");
-    
+
+    // Initial display of all trips
+    filterTrips('all');
+
+    // Handle filter change event
+    $("#trip-filter").change(function () {
+        const selectedFilter = $(this).val();
+        filterTrips(selectedFilter);
     });
-
-
-    
-    
-
-    
-
-    
+});
 
 
 
+function loadTripOptions() {
+    for (let i = 0; i < trips.length; i++) {
+        console.log(trips[i]);
+        $("#tripsContainer").append($("#tripCardTemplate").html());
+        let currentChild = $("#tripsContainer").children().eq(i + 1);
+        $(currentChild).find(".card-img-top").attr('src', '/assets/' + trips[i].image);
+        $(currentChild).find("#nameText").text(trips[i].name);
+        $(currentChild).find("#priceText").text('R ' + trips[i].price);
+        $(currentChild).find("#destinationText").text(trips[i].destination);
+        $(currentChild).find("#durationText").text(trips[i].duration);
+        $(currentChild).find("#dateOfDepartureText").text(trips[i].dateOfDeparture);
+        $(currentChild).find("#tripCodeText").text(trips[i].tripCode);
+    }
+}
 
 
+$("#plantsContainer").on('click', '.card', function () {
+    // Toggle the price & description text
+    $(this).find("#priceText").toggle();
+    $(this).find("#descriptionText").toggle();
+    // Resize the image to fit the additional content
+    $(this).find(".card-img-top").toggleClass("small");
+});
 
 // $(document).ready(function() {
 //     const tripContainer = $("#trip-container");
